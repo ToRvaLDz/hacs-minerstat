cb8523ea6cae9d356747f50b3f7f754d721318b0
Marco M <torvaldz@gmail.com>
2020-07-02 19:39:51 +0200

    Added currency converter

--- custom_components/hacs-minerstat/sensor.py
+++ custom_components/hacs-minerstat/sensor.py
@@ -10,6 +10,8 @@ __version__ = '1.0.0'
 CONF_NAME = 'name'
 CONF_ACCESS_KEY = 'access_key'
 CONF_RIG_NAME = 'rig_name'
+CONF_BASE_CURRENCY = 'EUR'
+
 DEFAULT_NAME = 'Minerstat'
 DEFAULT_SCAN_INTERVAL = timedelta(minutes=15)
 SCAN_INTERVAL = timedelta(minutes=15)
@@ -17,7 +19,8 @@ SCAN_INTERVAL = timedelta(minutes=15)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Required(CONF_ACCESS_KEY): str,
-    vol.Required(CONF_RIG_NAME): str
+    vol.Required(CONF_RIG_NAME): str,
+    vol.Required(CONF_BASE_CURRENCY): str
 })
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -29,6 +32,7 @@ class Minerstat(entity.Entity):
         self._config = config
         self._state = None
         self._unit = None
+        self._exchange = 1
         self.update()
 
     @property
@@ -44,13 +48,25 @@ class Minerstat(entity.Entity):
         return self._state
 
     def update(self):
+        self._unit=self._config[CONF_BASE_CURRENCY]
+
         req = urllib.request.Request(f'https://api.minerstat.com/v2/stats/{self._config[CONF_ACCESS_KEY]}/{self._config[CONF_RIG_NAME]}', headers={'User-Agent' : "Home-assistant.io"})
         with urllib.request.urlopen(req) as url:
             data = json.loads(url.read().decode())
+            self._usd = data[self._config[CONF_RIG_NAME]]['revenue']['usd_month']
+            self._cost = data[self._config[CONF_RIG_NAME]]['info']['electricity']
+            self._cons = data[self._config[CONF_RIG_NAME]]['info']['consumption']
 
-            self._state = data[self._config[CONF_RIG_NAME]]['revenue']['usd_month']
-            self._unit = 'usd'
+        self._exchange = 1
 
+        if self._config[CONF_BASE_CURRENCY]!='USD'
+            req = urllib.request.Request(f'https://api.exchangeratesapi.io/latest?base={self._config[CONF_BASE_CURRENCY]}&symbols=USD', headers={'User-Agent' : "Home-assistant.io"})
+            with urllib.request.urlopen(req) as url:
+                data = json.loads(url.read().decode())
+                self._exchange = data['rates']['USD']
+                
+        self._state = (self._usd / self._exchange)  - (self._cons/1000 * self._cost * 24 * 30)
+                
     @property
     def device_state_attributes(self):
         return {
